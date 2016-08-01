04bf5ce46187003f3b1114b97290a5b05c8e5c73
poluner <poluner@qq.com>
2016-08-01 21:02:08 +0800

    Create 最长上升子序列.cpp

---
+++ acm/dp/最长上升子序列.cpp
@@ -0,0 +1,25 @@
+#include<bits/stdc++.h>
+using namespace std;
+const int maxn=1000;
+const int inf=0x3f3f3f3f;
+
+int a[maxn+5];
+int g[maxn+5];
+int d[maxn+5];
+int main(){
+    int n;
+    cin>>n;
+    for(int i=1;i<=n;i++)
+        cin>>a[i];
+
+    memset(g,inf,sizeof(g));
+
+    for(int i=1;i<=n;i++){
+        int k=lower_bound(g+1,g+n+1,a[i])-g;
+        g[k]=a[i];
+        d[i]=k;
+    }
+
+    for(int i=1;i<=n;i++)
+        cout<<d[i]<<ends;
+}
